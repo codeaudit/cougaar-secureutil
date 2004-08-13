a6f0ba5b285b56ec90e56c1c93f45449ef3839c0
srosset <srosset>
2004-08-13 21:53:03 +0000

    Fixed event publishing issues.

--- securityutils/src/org/cougaar/core/security/monitoring/publisher/SecurityEventPublisher.java
+++ securityutils/src/org/cougaar/core/security/monitoring/publisher/SecurityEventPublisher.java
@@ -10,8 +10,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.cougaar.core.security.monitoring.event.FailureEvent;
 import org.cougaar.util.ConfigFinder;
@@ -26,7 +32,10 @@ import org.cougaar.util.log.LoggerFactory;
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
 public class SecurityEventPublisher {
-  private static List    m_eventTypes = new ArrayList();
+  /**
+   * A Map of Class (FailureEvent class) to Methods.
+   */
+  private static Map     m_eventTypes = new HashMap();
   private static List    m_events = new ArrayList();
   private static Logger  _log;
   
@@ -64,10 +73,30 @@ public class SecurityEventPublisher {
    * DataFailureEvent:       DataProtectionSensor
    */
   public static void publishEvent(FailureEvent event) {
-    synchronized(m_events) {
-        // Event listener has not been configured yet.
-        // Queue messages.
-        m_events.add(event);
+    Method m = (Method) m_eventTypes.get(event.getClass());
+    if (m != null) {
+      Object o[] = new Object[1];
+      o[0] = event;
+      try {
+        m.invoke(null, o);
+      } catch (IllegalArgumentException e) {
+        if (_log.isErrorEnabled()) {
+          _log.error("Unable to publish event: " + event);
+        }
+      } catch (IllegalAccessException e) {
+        if (_log.isErrorEnabled()) {
+          _log.error("Unable to publish event: " + event);
+        }
+      } catch (InvocationTargetException e) {
+        if (_log.isErrorEnabled()) {
+          _log.error("Unable to publish event: " + event);
+        }
+      }
+    }
+    else {
+      if (_log.isDebugEnabled()) {
+        _log.debug("Dropping event: " + event);
+      }
     }
   }
   
@@ -76,10 +105,54 @@ public class SecurityEventPublisher {
    */
   private static void readIdmefListeners(InputStream listenersIs) {
     BufferedReader br = new BufferedReader(new InputStreamReader(listenersIs));
+    Pattern p1 = Pattern.compile("(\\S*)\\s*(\\S*)");
     String line = null;
     try {
       while ( (line = br.readLine()) != null) {
-        
+        if (line.startsWith("#")) {
+          continue;
+        }
+        Matcher matcher = p1.matcher(line);
+        if (matcher.find()) {
+          Class eventClass = null;
+          Class sensorClass = null;
+          Method publishEventMethod = null;
+          String eventClassName = matcher.group(1);
+          if (eventClassName != null) {
+            try {
+              eventClass = Class.forName(eventClassName);
+            } catch (ClassNotFoundException e1) {
+              if (_log.isErrorEnabled()) {
+                _log.error("Unable to find event class name: " + eventClassName);
+              }
+            }
+          }
+          
+          String sensorClassName = matcher.group(2);
+          if (sensorClassName != null) {
+            try {
+              sensorClass = Class.forName(sensorClassName);
+              Class[] params = new Class[1];
+              params[0] = Class.forName("org.cougaar.core.security.monitoring.event.FailureEvent");
+              publishEventMethod = sensorClass.getDeclaredMethod("publishEvent", params);
+            } catch (ClassNotFoundException e1) {
+              if (_log.isErrorEnabled()) {
+                _log.error("Unable to find sensor class: " + eventClassName, e1);
+              }
+            } catch (SecurityException e) {
+              if (_log.isErrorEnabled()) {
+                _log.error("Unable to find sensor class: " + eventClassName, e);
+              }
+            } catch (NoSuchMethodException e) {
+              if (_log.isErrorEnabled()) {
+                _log.error("Unable to find sensor class: " + eventClassName, e);
+              }
+            }
+          }
+          if (eventClass != null && publishEventMethod != null) {
+            m_eventTypes.put(eventClass, publishEventMethod);
+          }
+        }
       }
     } catch (IOException e) {
       if (_log.isErrorEnabled()) {
